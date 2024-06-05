2d8264f210a2cd395b6cffd1883b28e01ef443ef
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-06-05 19:59:20 +0300

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -13,7 +13,7 @@ teks += `┃ ${wm}\n`
 teks += `╰━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.command = /^(منشن|invocar|invocacion|todos|invocación)$/i
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
