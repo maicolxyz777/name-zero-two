f5b4440290c739e33f5a5d5f087c1bc8bad16755
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-06-06 21:46:05 +0300

    Update descargas-apkpure.js

--- plugins/descargas-apkpure.js
+++ plugins/descargas-apkpure.js
@@ -42,7 +42,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
   }
 };
 
-handler.command = /^(apkp|apkpure|apkdl)$/i;
+handler.command = /^(تطبيق|apk|apkdl)$/i;
 handler.register = true;
 handler.limit = 2;
 export default handler;
