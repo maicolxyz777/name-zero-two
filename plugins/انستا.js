1a44bc3f8e76d78e40b8275f013e4fecbd41f678
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-06-09 23:54:48 +0300

    Create انستا.js

---
+++ plugins/انستا.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `✳️ ${mssg.useCmd}\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
+    m.react(rwait)
+
+try {
+    let res = await fetch(global.API('fgmods', '/api/downloader/igdl', { url: args[0] }, 'apikey'))
+    if (!res.ok) throw `❎ ${mssg.error} ` 
+    let data = await res.json()
+
+    for (let item of data.result) {
+        conn.sendFile(m.chat, item.url, 'igdl.jpg', `✅ تم التحميل`, m)
+    }
+  
+  
+    } catch (error) {
+        m.reply(`❎ ${mssg.error}`)
+    }
+    
+}
+handler.help = ['انستا <رابط الفيديو>']
+handler.tags = ['dl']
+handler.command = ['ig', 'igdl', 'instagram', 'igimg', 'igvid', 'انستا', 'إنستا', 'إنستجرام']
+handler.diamond = false
+
+export default handler
