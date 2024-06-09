dd669eb6e167af62652a0ede8fc8060f8eae3b1a
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-06-10 00:05:35 +0300

    Update انستا.js

--- plugins/انستا.js
+++ plugins/انستا.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `✳️ ${mssg.useCmd}\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
-    m.react(rwait)
+    m.react(⏳)
 
 try {
     let res = await fetch(global.API('fgmods', '/api/downloader/igdl', { url: args[0] }, 'apikey'))
